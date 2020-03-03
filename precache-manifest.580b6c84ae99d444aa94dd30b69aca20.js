a4e00a742fc08dd73979e84ad8264f3ce32372b7
tiffhuddleston <tiffhuddleston@gmail.com>
2020-03-02 20:30:48 -0600

    Updates

---
+++ precache-manifest.580b6c84ae99d444aa94dd30b69aca20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc9a9c822e87a64e3bf883908447705",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50a501b4245b041b00c3",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f82a54eabce715ec3025",
+    "url": "/static/css/main.46a3139b.chunk.css"
+  },
+  {
+    "revision": "50a501b4245b041b00c3",
+    "url": "/static/js/2.f9afde97.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.f9afde97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82a54eabce715ec3025",
+    "url": "/static/js/main.ca265cdd.chunk.js"
+  },
+  {
+    "revision": "4567734cd469a9475f6c",
+    "url": "/static/js/runtime-main.736723a6.js"
+  }
+]);
